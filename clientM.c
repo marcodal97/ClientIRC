b7506fb0f374717b8ee29f0275a10e1d8dd14627
marcodal97 <marcodal97@live.it>
2018-02-22 20:29:04 +0100

    client

--- clientM.c
+++ clientM.c
@@ -18,46 +18,102 @@
 
 int sock, msg_id;
 
-int main(int argc, char*argv[]){
+int main(){
+
+	msgctl(msg_id, IPC_RMID, 0);
 
 
-
-msg_id = msgget((key_t)MSGKEY, 0666 | IPC_CREAT);
-if(msg_id < 0){
-printf("Errore msgget\n");
-exit(1);
-}
+	msg_id = msgget((key_t)MSGKEY, 0666 | IPC_CREAT);
+		if(msg_id < 0){
+			printf("Errore msgget\n");
+			exit(1);
+		}
      
-tipo_coda coda;
+	tipo_coda coda;
 
 
-struct sockaddr_in server;
-struct hostent *hp, *gethostbyname();
-pthread_t threadW, threadR;
-
-sock = socket(AF_INET, SOCK_STREAM, 0);
-if(sock < 0){
-perror("Errore socket\n");
-exit(1);
-}
-
-hp = gethostbyname(argv[1]);
-
-server.sin_family = AF_INET;
-server.sin_port = htons(atoi(argv[2]));
-bcopy((char*)hp->h_addr, (char*)&server.sin_addr, hp->h_length);
-
-if(connect(sock, (struct sockaddr*)&server, sizeof(server))< 0){
-perror("Errore connect\n");
-exit(1);
-}
+	struct sockaddr_in server;
+	struct hostent *hp, *gethostbyname();
 
 
-pthread_create(&threadW, NULL, threadFW, (void*)&sock);
+	pthread_t threadW, threadR;
+
+
+//aspetto dati da visual
+
+	printf("Aspetto dati da visual\n");
+
+	if(msgrcv(msg_id, &coda, sizeof(tipo_coda)-sizeof(long int), 3, 0)<0){
+		perror("Errore msgrcv\n");
+		msgctl(msg_id, IPC_RMID, 0);
+		exit(1);
+	}
+
+	printf("Dati ricevuti\n");
+
+	printf("%s\n", coda.server);
+	printf("%d\n", coda.porta);
+
+	sock = socket(AF_INET, SOCK_STREAM, 0);
+	if(sock < 0){
+		perror("Errore socket\n");
+		exit(1);
+	}
+
+
+	hp = gethostbyname(coda.server);
+	printf("Provo connessione\n");
+	server.sin_family = AF_INET;
+	server.sin_port = htons(coda.porta);
+	bcopy((char*)hp->h_addr, (char*)&server.sin_addr, hp->h_length);
+
+
+
+	if(connect(sock, (struct sockaddr*)&server, sizeof(server))< 0){
+		perror("Errore connect\n");
+		coda.m_type = 1;
+		strcpy(coda.msg, "no");                  //se la connessione non riesce si fa sapere al visual che si chiude
+		msgsnd(msg_id, &coda, sizeof(tipo_coda)- sizeof(long int), 0);
+		msgctl(msg_id, IPC_RMID, 0);
+		exit(1);
+	}
+
+
+
+
+	printf("Connessione stabilita\n");
+
+//Mando conferma di connessione a visual
+
+	coda.m_type = 1;
+	strcpy(coda.msg, "ok");
+	if(msgsnd(msg_id, &coda, sizeof(tipo_coda) - sizeof(long int), 0) < 0){
+		perror("Errore msgsnd\n");
+		msgctl(msg_id, IPC_RMID, 0);
+		exit(1);
+	}
+
+	printf("Conferma inviata\n");
+
+
+	if(send(sock, coda.nick, strlen(coda.nick), 0)<0){
+		perror("Errore send\n");
+		msgctl(msg_id, IPC_RMID, 0);
+		exit(1);		
+	}
+
+
+
+
+
+
+
 pthread_create(&threadR, NULL, threadFR, (void*)&sock);
+pthread_create(&threadW, NULL, threadFW, (void*)&sock);
+
 
 pause();
-close(sock);
+
 }
 
 
@@ -67,31 +123,80 @@ void *threadFW(void *arg){
 
 int sockid = *(int*)arg;
 tipo_coda coda;
-
-	while(1){
-	
-	
-		memset(coda.mess.msg, '\0', MAX_BUF);
-
-		if(msgrcv(msg_id, &coda, sizeof(tipo_dato), 3, 0) < 0){
+/*
+	if(msgrcv(msg_id, &coda, sizeof(tipo_coda) - sizeof(long int), 3, 0) < 0){		//aspetto nick e user da visual
 		perror("Errore msgrcv\n");
 		msgctl(msg_id, IPC_RMID, 0);
 		exit(1);
 		}
 		
-		printf("Ric da vis\n");
-
-		if(send(sockid, coda.mess.msg, strlen(coda.mess.msg), 0)<0){
+		printf("Nick e user arrivati\n");
+		
+	if(send(sockid, coda.msg, strlen(coda.nick), 0)<0){   //invio nick e user a server
 		perror("Errore send\n");
 		msgctl(msg_id, IPC_RMID, 0);
 		exit(1);		
 		}
-		printf("Inv a serv %s\n", coda.mess.msg);
-		
-		if(strcmp(coda.mess.msg, "QUIT") == 0){
-		printf("Chiudo connessione\n");
+	if(send(sockid, coda.msg, strlen(coda.user), 0)<0){
+		perror("Errore send\n");
 		msgctl(msg_id, IPC_RMID, 0);
-		exit(0);
+		exit(1);		
+		}
+		
+	printf("Inviati\n");
+*/
+		
+		if(msgrcv(msg_id, &coda, sizeof(tipo_coda) - sizeof(long int), 3, 0) < 0){		//aspetto nick e user da visual
+		perror("Errore msgrcv\n");
+		msgctl(msg_id, IPC_RMID, 0);
+		exit(1);
+		}
+		
+		printf("%s", coda.nick);
+		printf("%s", coda.user);
+		
+		if(send(sockid, coda.nick, strlen(coda.nick), 0)<0){
+		perror("Errore send\n");
+		msgctl(msg_id, IPC_RMID, 0);									//li mando al server
+		exit(1);		
+		}
+		
+		
+		if(send(sockid, coda.user, strlen(coda.user), 0)<0){
+		perror("Errore send\n");
+		msgctl(msg_id, IPC_RMID, 0);
+		exit(1);	
+			
+		}
+		printf("Accesso\n");
+		
+		
+	while(1){						//tutto quello mandato dal visualizzatore lo invio al server
+	
+		memset(coda.msg, '\0', MAX_BUF);
+
+		if(msgrcv(msg_id, &coda, sizeof(tipo_coda) - sizeof(long int), 3, 0) < 0){
+			perror("Errore msgrcv\n");
+			msgctl(msg_id, IPC_RMID, 0);
+			exit(1);
+		}
+		
+		printf("Ric da vis\n");
+		
+	
+		
+		if(send(sockid, coda.msg, strlen(coda.msg), 0)<0){
+			perror("Errore send\n");
+			msgctl(msg_id, IPC_RMID, 0);
+			exit(1);		
+		}
+		
+		printf("Inv a serv %s\n", coda.msg);
+		
+		if(strcmp(coda.msg, "QUIT") == 0){
+			printf("Chiudo connessione\n");
+			msgctl(msg_id, IPC_RMID, 0);
+			exit(0);
 		}
 
 	}
@@ -101,25 +206,32 @@ tipo_coda coda;
 void *threadFR(void* arg){
 
 int sockid = *(int*)arg;
+
 tipo_coda coda;
 
+	
 
-	while(1){
+
+	while(1){  // tutto quello che viene inviato dal server viene inserito nella coda di messaggi
 	
 	
-	memset(coda.mess.msg, '\0', MAX_BUF);
+	memset(coda.msg, '\0', MAX_BUF);
 	
-	if(recv(sockid, &coda.mess.msg, MAX_BUF, 0) < 0){
-	printf("Errore recv\n");
-	msgctl(msg_id, IPC_RMID, 0);
-	exit(1);	
+	if(recv(sockid, &coda.msg, MAX_BUF, 0) < 0){
+		printf("Errore recv\n");
+		msgctl(msg_id, IPC_RMID, 0);
+		exit(1);	
 	}
 	
 	printf("Ric da ser \n");
 	coda.m_type = 1; //invio a visualizzatore il messaggio letto dal server
-	msgsnd(msg_id, &coda, sizeof(tipo_dato), 0);	
+	if(msgsnd(msg_id, &coda, sizeof(tipo_coda) - sizeof(long int), 0)< 0){
+		perror("Errore msgsnd\n");
+		msgctl(msg_id, IPC_RMID, 0);
+		exit(1);
+	}	
 	printf("Inv a vis\n");
-
+	
 
 	}
 
