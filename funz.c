c3269f6fd6a9d9f454398bec5475ac5e05fbf64c
marcodal97 <marcodal97@live.it>
2018-02-24 00:49:04 +0100

    Aggiunge funzioni per creare comandi

--- funz.c
+++ funz.c
@@ -45,7 +45,7 @@ int PONG(int sock){ //Quando arriva ping dal server si manda questa funzione in 
 
 int ris;
 
-str[MAX_BUF];
+char str[MAX_BUF];
 
 strcpy(str, "PONG");
 
@@ -57,6 +57,85 @@ ris = send(sock, str, strlen(str), 0);
 return ris;
 }
 
+
+
+
+void JOIN(char *comando){  //Crea comando per entrare in una stanza
+
+char stanza[MAX_BUX];
+
+printf("Inserisci nome stanza: \n");
+scanf("%s", stanza);
+
+memset(comando, '\0', MAX_BUF);
+
+strcpy(comando, "JOIN ");
+
+strcat(comando, stanza);
+
+comando[strlen(comando)] = '\n';
+comando[strlen(comando)+1] = '\0';
+
+}
+
+
+
+void PART(char *comando){  //Crea comando per lasciare stanza
+
+char stanza[MAX_BUX];
+
+printf("Inserisci nome stanza: \n");
+scanf("%s", stanza);
+
+memset(comando, '\0', MAX_BUF);
+
+strcpy(comando, "PART ");
+
+strcat(comando, stanza);
+
+comando[strlen(comando)] = '\n';
+comando[strlen(comando)+1] = '\0';
+
+
+
+}
+
+
+
+void MSG(char *comando, char *msg, char *nome){  //dato un messaggio e un canale/utente crea il comando per mandare il messaggio
+
+memset(comando, '\0', MAX_BUF);
+
+strcpy(comando, "PRIVMSG ");
+
+strcat(comando, nome);
+strcat(comando, " :");
+strcat(comando, msg);
+
+comando[strlen(comando)] = '\n';
+comando[strlen(comando)+1] = '\0';
+
+}
+
+
+void WHOIS(char *comando){  //Crea comando per avere informazioni su un utente
+
+char nick[MAX_BUF];
+
+printf("Inserisci nick\n");
+scanf("%s", nick);
+
+memset(comando, '\0', MAX_BUF);
+
+strcpy(comando, "WHOIS ");
+
+strcat(comando, nick);
+
+comando[strlen(comando)] = '\n';
+comando[strlen(comando)+1] = '\0';
+
+
+}
 
 
 
